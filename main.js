23328691a934a0d7ca5a6e0350704fa04c37b0af
Erik Albert Fetzer Szablevscki <103581739+ErikFsza@users.noreply.github.com>
2024-02-28 10:58:56 -0300

    ssa

--- main.js
+++ main.js
@@ -1,66 +1,11 @@
-</section>
-<script src="main.js">
-</body>
-</html>
-const botoes = document.querySelectorAll(".botao");
-console.log(botões);
 const botoes = document.querySelectorAll(".botao");
 
-for()
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;)
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    console.log();
-}
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick
-    console.log();
-}
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        botoes[i].classList.add("ativo");
+for (let i = 0; i < botoes.length; i++) {
+  console.log(i);
+  botoes[i].onclick = function() {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
     }
-    console.log();
-}
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-    }
-    console.log();
-}
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-    }
-}
-const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
-for(let i=0; i <  botões.length; i++){
-    console.log(i);
-}
-botoes[i].onclick = function(){
-    
-    botoes[i].classList.add(“ativo”);
-}
-for(let j=0;j<botoes.length;j++){
-    botoes[j].classList.remove("ativo");
+    botoes[i].classList.add("ativo");
+  };
 }
