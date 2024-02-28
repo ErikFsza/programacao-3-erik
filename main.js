0cdf019fb9f30cbd3411a046321fb6189a608301
Erik Albert Fetzer Szablevscki <103581739+ErikFsza@users.noreply.github.com>
2024-02-28 10:24:26 -0300

    er

---
+++ main.js
@@ -0,0 +1,54 @@
+</section>
+<script src="main.js">
+</body>
+</html>
+const botoes = document.querySelectorAll(".botao");
+console.log(botões);
+const botoes = document.querySelectorAll(".botao");
+
+for()
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;)
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
