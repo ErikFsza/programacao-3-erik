9087cf67debd6bf07895a525278ae188dfa73458
Erik Albert Fetzer Szablevscki <103581739+ErikFsza@users.noreply.github.com>
2024-03-06 10:49:55 -0300

    ..

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 
-for (let i = 0; i < botoes.length; i++) {
-  console.log(i);
-  botoes[i].onclick = function() {
-    for (let j = 0; j < botoes.length; j++) {
-      botoes[j].classList.remove("ativo");
-    }
-    botoes[i].classList.add("ativo");
-  };
-}
+botoes.forEach((botao, i) => {
+  botao.addEventListener("click", () => {
+    botoes.forEach((btn) => {
+      btn.classList.remove("ativo");
+    });
+    botao.classList.add("ativo");
+  });
+});
